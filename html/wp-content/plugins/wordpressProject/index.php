1e22569de7ab460a5a1aaafba42bd974968fa877
Yessenia Vela <vela.kathy@gmail.com>
2021-09-16 16:52:06 -0500

    configura con wpdb para crear un db en wordpress

--- html/wp-content/plugins/wordpressProject/index.php
+++ html/wp-content/plugins/wordpressProject/index.php
@@ -1,6 +1,6 @@
 <?php 
 /*
- * Plugin Name:       donationPlugin
+ * Plugin Name:       DonationPlugin
  * Plugin URI:        https://example.com/plugins/the-basics/
  * Description:       Handle the basics with this plugin.
  * Version:           1.10.3
@@ -8,6 +8,18 @@
 
 
 function Activation() {
+    // crea una tabla de bd desde wordpress
+    global $wpdb;
+
+    $sql = "CREATE TABLE IF NOT EXISTS {$wpdb->prefix}donaciones(
+        `DonacionId` INT NOT NULL AUTO_INCREMENT,
+        `Monto` INT NOT NULL,
+        `Nombre` VARCHAR(50) NULL,
+        `Email` VARCHAR(50) NULL,
+        `Telefono` INT NOT NULL,
+        PRIMARY KEY (`DonacionId`));";
+
+    $wpdb->query($sql);
 }
 
 function Deactivation() {
@@ -36,23 +48,17 @@ function CrearMenu(){
 
 function example_plugin()
 {
-
 	/* create a variable to hold the html information */
 	$content = ''; /* creates a string variable */
-
 	/* open the form tag */
 	$content .= '<form method="post" action="'.plugin_dir_path(__FILE__).'admin/processed.php">'; /* adds to the string variable */
-
-	$content .= '<input type="number" name="Importe" placeholder="Monto a aportar" /><br /><br />';
-	$content .= '<input type="text" name="your_name" placeholder="Nombre completo" /><br /><br />';
+	$content .= '<input require type="number" name="Importe" placeholder="Monto a aportar" /><br /><br />';
+	$content .= '<input type="text" name="your_name" placeholder="Nombre" /><br /><br />';
+    $content .= '<input type="text" name="your_last_name" placeholder="Apellido" /><br /><br />';
 	$content .= '<input type="email" name="your_email" placeholder="Email" /><br /><br />';
     $content .= '<input type="number" name="phone" placeholder="Número de teléfono" /><br /><br />';
-    $content .= '<input type="number" name="card" placeholder="Número de tarjeta" /><br /><br />';
-    $content .= '<input type="date" name="expiration" placeholder="Fecha de exp." /><br /><br />';
-    $content .= '<input type="number" name="cvv" placeholder="CVV" /><br /><br />';
 
 	$content .= '<input type="submit" name="form_exaple_contact_submit" value="DONAR" /><br /><br />';
-
 	/* close the form tag */
 	$content .= '</form>';
 
