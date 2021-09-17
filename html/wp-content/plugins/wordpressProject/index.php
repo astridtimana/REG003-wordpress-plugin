89235e910ead90910d979b166960a347324d612f
astridtimana <astrid.1295@gmail.com>
2021-09-17 17:52:58 -0500

    db & culqi checkout

--- html/wp-content/plugins/wordpressProject/index.php
+++ html/wp-content/plugins/wordpressProject/index.php
@@ -12,17 +12,29 @@ Licence: MIT
 
 if (!defined('ABSPATH')) exit;
 
-function Activate(){
+function Activation() {
+    // crea una tabla de bd desde wordpress
+    global $wpdb;
+
+    $sql = "CREATE TABLE IF NOT EXISTS {$wpdb->prefix}donaciones(
+        `DonacionId` INT NOT NULL AUTO_INCREMENT,
+        `Monto` INT NOT NULL,
+        `Nombre` VARCHAR(50) NULL,
+        `Email` VARCHAR(50) NULL,
+        `Telefono` INT NOT NULL,
+        PRIMARY KEY (`DonacionId`));";
+
+    $wpdb->query($sql);
 }
 
-function Deactivate(){
-	flush_rewrite_rules();
+function Deactivation() {
+    flush_rewrite_rules();
 }
 
-echo "Hola soy plugin";
 
-register_activation_hook(__FILE__, 'Activate');
-register_deactivation_hook(__FILE__, 'Deactivate');
+register_activation_hook(__FILE__, 'Activation');
+register_deactivation_hook(__FILE__, 'Deactivation');
+
 
 add_action('admin_menu', 'CreateMenu');
 
@@ -59,6 +71,7 @@ function CreateMenu() {
 
 
 add_shortcode('ShortcodeDonate', 'ShortcodeDonation');
+
 function ShortcodeDonation($atts) {
     //attributes
     $atts = shortcode_atts(
@@ -71,15 +84,26 @@ function ShortcodeDonation($atts) {
     . $atts[title_text3] .
         '</h2><br/>
         <form method="post" action="" style="text-align: center;">
-            <input type="number" name="Importe" placeholder="Monto a aportar" style="border-radius: 10px; border: none; outline: none; width: 100%"/><br /><br />
+            <input type="number" name="importe" placeholder="Monto a aportar" style="border-radius: 10px; border: none; outline: none; width: 100%"/><br /><br />
             <input type="text" name="your_name" placeholder="Nombre completo" style="border-radius: 10px; border: none;  outline: none; width: 100%" /><br /><br />
             <input type="email" name="your_email" placeholder="Email" style="border-radius: 10px; border: none;  outline: none ; width: 100%" /><br /><br />
             <input type="number" name="phone" placeholder="Número de teléfono" style="border-radius: 10px; border: none;  outline: none; width: 100%" /><br /><br />
-            <input type="submit" name="form_exaple_contact_submit" value="DONAR" style="border-radius: 10px; border: none; color: #362277; font-weight: bolder;background: #abe1c1;  outline: none ; width: 100%" /><br /><br />
+            <input type="submit" name="submit" value="DONAR" style="border-radius: 10px; border: none; color: #362277; font-weight: bolder;background: #abe1c1;  outline: none ; width: 100%" /><br /><br />
         </form>
+        <a href="https://checkout.culqi.com/js/v3"><button>GO</button></a>
+       
     </div>
     ';
+    
 }
+
+    //isset -> evalua si una variable está definida - se ha mandado un formulario?
+	if(isset($_POST['submit'])){
+		$campos = array("Monto"=>$_POST['importe'], "Nombre"=>$_POST['your_name'], "Email"=>$_POST['your_email'], "Telefono"=>$_POST['phone']);
+		$tabla = "wp_donaciones";//Tabla en base de datos
+		$wpdb->insert($tabla,$campos);
+		
+	}
 
 
 ?>
