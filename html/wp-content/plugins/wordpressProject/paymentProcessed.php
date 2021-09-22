ddcec9884128177191e4cec9a6814161b625c758
carlasanchezcorrea <carlasanchezcorrea@gmail.com>
2021-09-22 05:05:06 -0500

    payment

--- html/wp-content/plugins/wordpressProject/paymentProcessed.php
+++ html/wp-content/plugins/wordpressProject/paymentProcessed.php
@@ -1,4 +1,5 @@
 <?php
+error_log('22');
 // Cargamos Requests y Culqi PHP
 include_once dirname(__FILE__).'/Requests/library/Requests.php';
 Requests::register_autoloader();
@@ -8,22 +9,26 @@ include_once dirname(__FILE__).'/culqi-php/lib/culqi.php';
 // Configurar tu API Key y autenticación
 $SECRET_KEY = "pk_test_87a7198984bae065";
 $culqi = new Culqi\Culqi(array('api_key' => $SECRET_KEY));
+// echo $_POST['amount2'];
+// echo $_POST['description2'];
+// echo $_POST['email2'];
+// echo $_POST['token'];
 
 // Creando cargo
-$dataM = array(
-    "amount" => $_POST['amount'],
-    "currency_code" => "PEN",
-    "description" => $_POST['description'],
-    "email" => $_POST['email'],
-    "source_id" => $_POST['token'],
-);
+// $charge = $culqi->Charges->create(
+//     array(
+//         "amount" => $_POST['amount2'],
+//         "currency_code" => "PEN",
+//         "description" => $_POST['description2'],
+//         "email" => $_POST['email2'],
+//         "source_id" => $_POST['token'],
+//         )
+//     );
 
-echo $dataM;
-// Creando cargo
-$charge = $culqi->Charges->create($dataM);
 echo '¡Donación Exitosa!';
 
 //Respuesta
-print_r($charge);
+// print_r($charge);
+
 
 ?>
