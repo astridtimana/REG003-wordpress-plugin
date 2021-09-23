da41fd6bd669a77183c0d50c3881ef46759754d0
Yessenia Vela <vela.kathy@gmail.com>
2021-09-23 18:28:21 -0500

    cambia orden de líneas de bd

--- html/wp-content/plugins/wordpressProject/paymentProcessed.php
+++ html/wp-content/plugins/wordpressProject/paymentProcessed.php
@@ -3,30 +3,15 @@ include_once('../../../wp-config.php');
 include_once('../../../wp-load.php');
 include_once('../../../wp-includes/wp-db.php');
 
-// global $wpdb;
 try {
 
     //include_once dirname(__FILE__).'/Requests/library/Requests.php';
     Requests::register_autoloader();
     include_once dirname(__FILE__).'/culqi-php/lib/culqi.php';
 
-    //if(isset($_POST["submit"])){
-    $campos = array(
-        "Monto"=>$_POST['amount'], 
-        "Nombre"=>$_POST['name'], 
-        "Email"=>$_POST['email'], 
-        "Telefono"=>$_POST['phone']);
-    $tabla = 'wp_donaciones';//Tabla en base de datos
-    $wpdb->insert($tabla,$campos);
-    //}
-
-    // // Configurar tu API Key y autenticación
+    // Configurar tu API Key y autenticación
     $SECRET_KEY = "sk_test_7d9f4a5fe70f8315";
     $culqi = new Culqi\Culqi(array('api_key' => $SECRET_KEY));
-    // echo $_POST['amount'];
-    // echo $_POST['description'];
-    // echo $_POST['email'];
-    // echo $_POST['token'];
 
     // Creando cargo
     $charge = $culqi->Charges->create(
@@ -40,6 +25,14 @@ try {
         );
   
     echo '¡Donación exitosa!';
+
+    $campos = array(
+        "Monto"=>$_POST['amount'], 
+        "Nombre"=>$_POST['name'], 
+        "Email"=>$_POST['email'], 
+        "Telefono"=>$_POST['phone']);
+    $tabla = 'wp_donaciones';//Tabla en base de datos
+    $wpdb->insert($tabla,$campos);
 
 } catch (Exception $e) {
     echo "catch";
