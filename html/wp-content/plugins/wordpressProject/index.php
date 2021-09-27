fc84ee293f1d4587edbb7d3dc6968172f3dfa62e
carlasanchezcorrea <carlasanchezcorrea@gmail.com>
2021-09-27 05:17:19 -0500

    console.log

--- html/wp-content/plugins/wordpressProject/index.php
+++ html/wp-content/plugins/wordpressProject/index.php
@@ -1,109 +1,115 @@
 <?php
+// ----------- 1° 	TO DO	Add description Pluging
 /*
-Plugin Name: plugin donation
+Plugin Name: Plugin Donation
 Plugin URI: https://example.com/plugin-name
 Description: Este un plugin para donación de dinero a organizaciones sin fines de lucro y puede ser integrado con la pasarela de pago Culqi.
 Version: 0.0.1
 Requires at Least: 5.6.1
 Requires PHP: 7.4.14
-Author: Astrid & Mery
+Author: Yessenia Vela & Carla Sánchez
 Licence: MIT
 */
-//require
-require_once dirname(__FILE__) . '/classes/shortcode.class.php';
+
+require_once dirname(__FILE__) . '/classes/shortcode.class.php'; //call functions from class into this file
 
 
 if (!defined('ABSPATH')) exit;
+// ----------- 2° 	TO DO	Add Plugin Hook Activation
 
-function Activation() {
-    // crea una tabla de bd desde wordpress
-    global $wpdb;
+// ---------Function Activation Plugin 	
+	function Activation() {
+		global $wpdb; //SQL statements - global $wpdb object - global variable
+		//Use comands SQL
+		//Create DB_TableName => 'wp_donaciones' (
+		// 1° nameUniqueKey		-	data_type		-	NOT NULL OR NULL AND/OR AUTO_INCREMENT
+		// 2° property_name
+		// INT => 'number not decimal'	-	VARCHAR=>'string' =>VARCHAR(number=>'lenght')
+		$sql = "CREATE TABLE IF NOT EXISTS {$wpdb->prefix}donaciones(
+			`DonacionId` 	INT 			NOT NULL AUTO_INCREMENT,
+			`Monto` 		INT 			NOT NULL,
+			`Nombre` 		VARCHAR(50) 	NULL,
+			`Email` 		VARCHAR(50) 	NULL,
+			`Telefono` 		INT 			NOT NULL,
+			PRIMARY KEY (`DonacionId`));";
 
-    $sql = "CREATE TABLE IF NOT EXISTS {$wpdb->prefix}donaciones(
-        `DonacionId` 	INT 			NOT NULL AUTO_INCREMENT,
-        `Monto` 		INT 			NOT NULL,
-        `Nombre` 		VARCHAR(50) 	NULL,
-        `Email` 		VARCHAR(50) 	NULL,
-        `Telefono` 		INT 			NOT NULL,
-        PRIMARY KEY (`DonacionId`));";
+		$wpdb->query($sql);
+		//Create DB_TableName => 'wp_settings' 
+		$settingsTable = "CREATE TABLE IF NOT EXISTS {$wpdb->prefix}settings(
+			`SettingsId` 	INT 			NOT NULL AUTO_INCREMENT,
+			`SecretKey` 	VARCHAR(40)		NOT NULL,
+			`PublicKey` 	VARCHAR(40) 	NULL,
+			`OrgName` 		VARCHAR(40) 	NULL,
+			PRIMARY KEY (`SettingsId`));";
 
-    $wpdb->query($sql);
+		$wpdb->query($settingsTable);
+	}
 
-	$settingsTable = "CREATE TABLE IF NOT EXISTS {$wpdb->prefix}settings(
-        `SettingsId` 	INT 			NOT NULL AUTO_INCREMENT,
-		`SecretKey` 	VARCHAR(40)		NOT NULL,
-		`PublicKey` 	VARCHAR(40) 	NULL,
-		`OrgName` 		VARCHAR(40) 	NULL,
-        PRIMARY KEY (`SettingsId`));";
-
-    $wpdb->query($settingsTable);
-}
 
 function Deactivation() {
     flush_rewrite_rules();
 }
 
+//register_activation_hook => WordPress's Function
+register_activation_hook(__FILE__, 'Activation'); //wrdpss_function('_file_', function_name')
+register_deactivation_hook(__FILE__, 'Deactivation'); //wrdpss_function('_file_', function_name')
 
-register_activation_hook(__FILE__, 'Activation');
-register_deactivation_hook(__FILE__, 'Deactivation');
-
-
-add_action('admin_menu', 'CreateMenu');
-
-function CreateMenu() {
-	add_menu_page(
-		'Instrucciones de uso del plugin Menú', //pageTitle
-		'DonationPlugin',//menuTitle
-		'manage_options',//capability
-		plugin_dir_path(__FILE__) . 'admin/mainpage.php',//menuSlug
-		null, //functionName
-		'1' //position
-	);
-	add_submenu_page(
-		plugin_dir_path(__FILE__) . 'admin/mainpage.php',//menuSlug
-		'Submenu 1',
-		'Historial de Donaciones',
-		'manage_options',
-		plugin_dir_path(__FILE__) . 'admin/tablaDonacion.php',
-		null,
-		'2'
-	);
-	add_submenu_page(
-		plugin_dir_path(__FILE__) . 'admin/mainpage.php',//menuSlug
-		'Submenu 2',
-		'Settings Culqi',
-		'manage_options',
-		plugin_dir_path(__FILE__) . 'admin/settings.php',
-		null,
-		'3'
-	);
+// ----------- 3° 	TO DO	Confing menu section plugin
+// --------------------------Config menu plugging-----------------------
+	add_action('admin_menu', 'CreateMenu');
+	function CreateMenu() {
+	// #region-----------------Principal file Position #1----------------
+		add_menu_page(
+			'Instrucciones de uso del plugin Menú', //pageTitle
+			'DonationPlugin', //menuTitle
+			'manage_options', //capability
+			plugin_dir_path(__FILE__) . 'admin/mainpage.php', //menuSlug-path file
+			null, //functionName
+			'1' //position
+		);
+	// #region----------------------Option Position #2-------------------
+		add_submenu_page(
+			plugin_dir_path(__FILE__) . 'admin/mainpage.php', //menuSlug
+			'Submenu 1',
+			'Historial de Donaciones', //menuTitle
+			'manage_options', //capability
+			plugin_dir_path(__FILE__) . 'admin/tablaDonacion.php', //menuSlug-path file
+			null,
+			'2' //position
+		);
+	// #region----------------------Option Position #3-------------------
+		add_submenu_page(
+			plugin_dir_path(__FILE__) . 'admin/mainpage.php',//menuSlug
+			'Submenu 2',
+			'Settings Culqi', //menuTitle
+			'manage_options', //capability
+			plugin_dir_path(__FILE__) . 'admin/settings.php', //menuSlug-path file
+			null,
+			'3' //position
+		);
 }
 
+// --------------------------ShortCode Form Register Data----------------
+	// add_shortcode=> WordPress's function 'prefixing '
+	// wordpress_function('title_name_to_call' , 'function_name')
+	add_shortcode('ShortcodeDonate', 'ShortcodeDonation');
+	function ShortcodeDonation($atts) {
+		$_short = new shortCode; // call 'class'
+		$title = $atts['title']; //attributes
+		$html = $_short->formulario($title); // call function into class & add param
+		
+		return $html;
+	}
 
-
-add_shortcode('ShortcodeDonate', 'ShortcodeDonation');
-
-function ShortcodeDonation($atts) {
-    //attributes
-    $_short = new shortCode;
-  $title = $atts['title'];
-
-  $html = $_short->formulario($title);
-  
-  return $html;
-
-}
-
-
-//enviando información del Webmaster
-if(isset($_POST['submitWebmasterInfo'])){
-	$webmasterData = array("SecretKey"=>$_POST['secretKey'], "PublicKey"=>$_POST['publicKey'], "OrgName"=>$_POST['orgName']);
-	$tabla = "wp_settings";//Tabla en base de datos
-	$wpdb->insert($tabla,$webmasterData);
-
-}
-
-
-
+// ------------------Send Data from Webmaster to DataBase----------------
+	//isset => true or false depends on the variable
+	//$_POST => access the information sent from the method 'Post'
+	//$_POST['form_button_submit_nameAttribute_']
+	if(isset($_POST['submitWebmasterInfo'])){
+		//create array with 'property name'=> 'params'
+		$webmasterData = array("SecretKey"=>$_POST['secretKey'], "PublicKey"=>$_POST['publicKey'], "OrgName"=>$_POST['orgName']);
+		$tabla = "wp_settings"; //call DB_TableName
+		$wpdb->insert($tabla,$webmasterData); //finally 'insert this array to this DB_TableName'
+	}
 
 ?>
